d58fda4122824aa4d925713746dc74fb92a2874b
Oleksandr Zinchenko <zinchenko@live.com>
2018-11-20 10:48:57 -0800

    async file write. align html markdown

--- simple-server/server.js
+++ simple-server/server.js
@@ -14,9 +14,14 @@ const log = (name, data) => {
 // request: GET /
 const rootHandler = (req, res) => {
     res.write('<html>');
-    res.write('<head><title>Put your dirty message here:</title></head>');
+    res.write('<head> \
+                 <title>Put your dirty message here:</title> \
+               </head>');
     res.write('<body>');
-    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+    res.write('<form action="/message" method="POST"> \
+                 <input type="text" name="message"> \
+                 <button type="submit">Send</button> \
+               </form>');
     res.write('</body>');
     res.write('</html>');
     res.end();
@@ -24,11 +29,19 @@ const rootHandler = (req, res) => {
 
 // request: POST /message
 const messageHandler = (req, res) => {
-    fs.writeFileSync('message.txt', '<>');
-
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    res.end();
+    const chunks = [];
+    req.on('data', data => {
+        chunks.push(data);
+    });
+    req.on('end', () => {
+        const data    = Buffer.concat(chunks);
+        const message = data.toString().split('=')[1];
+        fs.writeFile('message.txt', message, err => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            res.end();
+        });
+    });
 }
 
 const PATH    = 0;
@@ -56,16 +69,19 @@ const httpServer = http.createServer((req, res) => {
             pathRoute[METHOD] === req.method) {
                 pathRoute[HANDLER](req, res);
                 handledBy = pathRoute;
-                res.end();
                 break;
         }
 
     if (!handledBy) {
         res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>O_o</title></head>');
-        res.write('<body><h1>This way of writing HTML is super ugly... You should try something else.</h1></body>');
-        res.write('</html>');
+        res.write('<html> \
+                     <head> \
+                       <title>O_o</title> \
+                     </head> \
+                     <body> \
+                       <h1>This way of writing HTML is super ugly... You should try something else.</h1> \
+                     </body> \
+                   </html>');
         res.end();
     }
 });
