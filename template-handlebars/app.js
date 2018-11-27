18cf50294f57373be5be35a9670b83783d3caeeb
Oleksandr Zinchenko <zinchenko@live.com>
2018-11-27 14:58:43 -0800

    Handlebars template engine

---
+++ template-handlebars/app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const expressHbs = require('express-handlebars');
+
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/message', (req, res, next) => {
+    res.render('message',
+        { pageTitle: "Input your message"
+        , lbMessage: "Message:"
+        , btnSend: "Send message" })
+});
+
+app.post('/message', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/message');
+});
+
+app.get('/', (req, res, next) => {
+    res.redirect('/message');
+});
+
+app.listen(4000);
