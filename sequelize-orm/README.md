93aebcd5e3c9a3b29227ff04ac4c324dde128024
Oleksandr Zinchenko <zinchenko@live.com>
2018-11-29 17:09:40 -0800

    API and readme

--- sequelize-orm/README.md
+++ sequelize-orm/README.md
@@ -1,9 +1,26 @@
 # sequelize-orm
 
 ```bash
-# start detached mssql db (and adminer)
+# start detached mssql db
 docker-compose up -d
 
 # run the app
 npm start
+
+# post some product
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+            "title": "NES Classic",
+            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/9/9e/Nintendo_CLV-101_20170617.jpg",
+            "price": 49.99,
+            "description": "Dedicated video game console by Nintendo, which emulates the Nintendo Entertainment System (NES)."
+          }' \
+  http://localhost:3000/api/product
+
+# get the list of products
+curl http://localhost:3000/api/product/all
+
+# get product by id
+curl http://localhost:3000/api/product/1
 ```
--- sequelize-orm/app.js
+++ sequelize-orm/app.js
@@ -2,7 +2,53 @@ const express = require('express');
 const app = express();
 
 const db = require('./db');
-const product = require('./models/product');
+const Product = require('./models/product');
+
+app.use(express.json());
+
+app.post('/api/product', (req, res, next) => {
+    const title       = req.body.title;
+    const imageUrl    = req.body.imageUrl;
+    const price       = req.body.price;
+    const description = req.body.description;
+
+    Product.create({
+        title:       title,
+        price:       price,
+        imageUrl:    imageUrl,
+        description: description
+    })
+    .then(result => {
+        res.status(200).send('OK');
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send("Oops");
+    });
+});
+
+app.get('/api/product/all', (req, res, next) => {
+    Product.findAll()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Err!');
+        });
+});
+
+app.get('/api/product/:productId', (req, res, next) => {
+    const prodId = req.params.productId;
+
+    Product.findByPk(prodId)
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            res.status(500).send('Ahrr!');
+        });
+});
 
 db.sync()
   .then(result => {
@@ -10,5 +56,6 @@ db.sync()
       app.listen(3000);
   })
   .catch(err => {
-    console.log(err);
+      console.log('Could not connect to the db, terminating...');
+      console.log(err);
   });
