b67b296892420c18fe1aa1295ceee908d6b30cf6
Oleksandr Zinchenko <zinchenko@live.com>
2018-11-30 16:37:04 -0800

    mongoose product model, get and post

--- mongoose-odm/README.md
+++ mongoose-odm/README.md
@@ -6,4 +6,18 @@ docker-compose up -d
 
 # run the app
 npm start
+
+# create the product
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{
+            "title": "NES Classic",
+            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/9/9e/Nintendo_CLV-101_20170617.jpg",
+            "price": 49.99,
+            "description": "Dedicated video game console by Nintendo, which emulates the Nintendo Entertainment System (NES)."
+          }' \
+  http://localhost:3000/api/products
+
+# get the list of all products
+curl http://localhost:3000/api/products
 ```
--- mongoose-odm/app.js
+++ mongoose-odm/app.js
@@ -1,5 +1,71 @@
 const express = require('express');
 const app = express();
 
+const Product = require('./models/product');
 
-app.listen(3000);
+const mongoose = require('mongoose');
+const conStr   = 'mongodb://localhost:27017/mongoose-db';
+const authOpt  = {
+    user: 'root',
+    pass: 'example',
+    auth: {
+        authdb: 'admin'
+    }
+};
+
+app.use(express.json());
+
+app.post('/api/products', (req, res, next) => {
+    const title       = req.body.title;
+    const imageUrl    = req.body.imageUrl;
+    const price       = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product({
+        title: title,
+        price: price,
+        description: description,
+        imageUrl: imageUrl
+    });
+
+    product.save()
+        .then(result => {
+            res.status(200).send(JSON.stringify({
+                id: result._id
+            }));
+        })
+        .catch(err => {
+            res.status(500).send('Err!');
+        });
+});
+
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+        .then(products => {
+            res.send(products);
+        })
+        .catch(err => {
+            res.status(500).send('Ahr!');
+        });
+});
+
+app.get('/api/products/:productId', (req, res, next) => {
+    const productId = req.params.productId;
+
+    Product.findById(productId)
+        .then(product => {
+            res.send(product);
+        })
+        .catch(err => {
+            res.status(500).send('Bang!');
+        });
+});
+
+mongoose.connect(conStr, authOpt)
+    .then(result => {
+        console.log('The app is running on 3000...');
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
