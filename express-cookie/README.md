0700536489c3a376748ab7e2d4c18304eff31465
Oleksandr Zinchenko <zinchenko@live.com>
2018-12-03 13:44:59 -0800

    get and set auth cookies

--- express-cookie/README.md
+++ express-cookie/README.md
@@ -2,11 +2,19 @@
 
 ```bash
 
-# get the cookie
+# get the auth cookie
 curl -i \
      --header "Content-Type:application/json; charset=UTF-8" \
      --request POST \
+     --cookie-jar cookies.txt \
      --data '{"login":"admin", "password":"pwd"}' \
-     http://localhost:3000/api/login
+     http://localhost:3000/api/v1/login
+
+# send the auth cookie back
+curl -i \
+    --request GET \
+    --cookie cookies.txt \
+    http://localhost:3000/api/v1/resource
+
 
 ```
--- express-cookie/app.js
+++ express-cookie/app.js
@@ -3,20 +3,34 @@ const app     = express();
 
 app.use(express.json());
 
-app.post('/api/login', (req, res, next) => {
+app.post('/api/v1/login', (req, res, next) => {
     console.log(req.body);
 
     const login    = req.body.login;
     const password = req.body.password;
 
     if (login == 'admin' && password == 'pwd') {
-        // who needs JWT ^_^ ?
+        // who needs JWT and some third-party libraries ^_^ ?
         // security? never heard of it...
-        res.setHeader('Set-Cookie', 'Authenticated=True; Expires=Wed, 13 Jan 2029 20:20:01 GMT; HttpOnly;');
-        res.setHeader('Set-Cookie', 'Role=Admin');
+        res.setHeader('Set-Cookie', 'Authenticated=True');
         res.status(200).send('OK');
     } else {
         res.status(422).send('Invalid credentials');
+    }
+});
+
+app.get('/api/v1/resource', (req, res, next) => {
+    const cookie = req.get('Cookie');
+    console.log(cookie);
+
+    // You should never write the code like this.
+    // It's only for demonstration purposes and still hurts my eyes.
+    const isAuth = cookie == undefined ? false : cookie.split('=')[1];
+
+    if (isAuth) {
+        res.status(200).send('OK');
+    } else {
+        res.status(401).send('Unauthorized');
     }
 });
 
