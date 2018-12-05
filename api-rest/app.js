3bec33e4f433664532f32a4dd179107b0c23a5f9
Oleksandr Zinchenko <zinchenko@live.com>
2018-12-05 13:52:39 -0800

    CORS headers

--- api-rest/app.js
+++ api-rest/app.js
@@ -4,6 +4,12 @@ const app     = express();
 const messageRoutes = require('./routes/message');
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/messages', messageRoutes);
 
