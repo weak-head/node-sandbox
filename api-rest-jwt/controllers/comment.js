46f237b2f0572d46bfd1268eecb40ee19e5df026
Oleksandr Zinchenko <zinchenko@live.com>
2018-12-05 23:23:41 -0800

    rest jwt api: request validation

--- api-rest-jwt/controllers/comment.js
+++ api-rest-jwt/controllers/comment.js
@@ -1,9 +1,25 @@
-exports.getMessages = (req, res, next) => {
+const { validationResult } = require('express-validator/check');
+
+exports.getComments = (req, res, next) => {
     console.log('get');
-    res.json('ok');
+
+    res.status(200).json('ok');
 };
 
-exports.postMessage = (req, res, next) => {
-    console.log('post');
-    res.json('ok');
+exports.postComment = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed',
+            errors: errors.array()
+        });
+    }
+
+    const title   = req.body.title;
+    const content = req.body.content;
+
+    console.log(`post ${title} -> ${content}`);
+
+    res.status(201).json('ok');
 };
--- api-rest-jwt/routes/comment.js
+++ api-rest-jwt/routes/comment.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router  = express.Router();
 
+const { body } = require('express-validator/check');
+
 const commentController = require('../controllers/comment');
 
-router.get('/', commentController.getMessages);
-router.post('/create', commentController.postMessage);
+router.get('/', commentController.getComments);
+
+router.post('/create', [
+    body('title').trim().isLength({min: 5}),
+    body('content').trim().isLength({min: 10})
+], commentController.postComment);
 
 module.exports = router;
