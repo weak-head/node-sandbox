c92c4f1ef7c2b23bd1158925c9267b128832fd64
Oleksandr Zinchenko <zinchenko@live.com>
2018-12-05 16:38:31 -0800

    REST API: post

--- api-rest/controllers/message.js
+++ api-rest/controllers/message.js
@@ -1,4 +1,5 @@
 exports.getMessages = (req, res, next) => {
+    console.log('get messages');
     res.status(200).json({
         messages: [{ title: 'Deeds', body: 'Have been settled' }]
     });
@@ -8,6 +9,7 @@ exports.postMessage = (req, res, next) => {
     const title = req.body.title;
     const body  = req.body.body;
 
+    console.log(`create message: [${title}] - [${body}]`);
     res.status(201).json({
         title: title,
         body: body
--- api-rest/frontend/public/main.js
+++ api-rest/frontend/public/main.js
@@ -9,3 +9,23 @@ getButton.addEventListener('click', () => {
         })
         .catch(err => console.log(err));
 });
+
+postButton.addEventListener('click', () => {
+    fetch('http://localhost:8080/messages/create', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'message title',
+            body: 'message body'
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(resData => {
+            console.log(resData);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
