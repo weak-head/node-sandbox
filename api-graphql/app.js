2416912b3d3fc80eb9592b4a6ef30136b5273cb0
Oleksandr Zinchenko <zinchenko@live.com>
2018-12-13 12:09:58 -0800

    Handle OPTIONS request

--- api-graphql/app.js
+++ api-graphql/app.js
@@ -10,6 +10,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // GraphQL only supports GET and POST requests,
+  // so we need to handle OPTIONS ourselves
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
